88866f13e17667412de88d52766289cd87721421
Reuben John <reubenvjohn@gmail.com>
2020-03-02 01:25:24 +0300

    Closes #12 Ant is not distracted by markers behind it.

--- Assets/Sensors/CircleOverlapSensor.cs
+++ Assets/Sensors/CircleOverlapSensor.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public abstract class ProximitySensor : MonoBehaviour
 {
     public abstract string ID { get; }
-    public abstract Collider2D[] Sense();
+    public abstract IEnumerable<Collider2D> Sense();
 }
 
 public class CircleOverlapSensor : ProximitySensor
@@ -13,17 +14,24 @@ public class CircleOverlapSensor : ProximitySensor
     public string id;
     public float range = 1;
     public LayerMask layerMask = 0;
+    public float angle = 180;
 
     public override string ID => id;
 
-    public override Collider2D[] Sense()
+    public override IEnumerable<Collider2D> Sense()
     {
-        return Physics2D.OverlapCircleAll(transform.position, range, layerMask);
+        return Physics2D.OverlapCircleAll(transform.position, range, layerMask)
+            .Where(collider =>
+            {
+                float vectorAngle = Vector2.Angle(transform.right, collider.transform.position - transform.position);
+                return vectorAngle < angle / 2;
+            });
     }
 
     private void OnDrawGizmosSelected()
     {
         UnityEditor.Handles.color = Color.green;
-        UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.back, range);
+        UnityEditor.Handles.DrawWireArc(transform.position, transform.forward, transform.right, -angle / 2, range);
+        UnityEditor.Handles.DrawWireArc(transform.position, transform.forward, transform.right, angle / 2, range);
     }
 }
